53165bbc3ab9c57db1e5f12123887f1718b79770
gOmmeG <153539148+gOmmeG@users.noreply.github.com>
2025-02-17 23:45:45 +0000

    Atualização de erros

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,7 +12,7 @@ int main() {
     
     char codigo1[4], codigo2[4], codigo3[4], codigo4[4], nome1 [20], nome2 [20], nome3 [20], nome4[20];
     int populacao1, populacao2, populacao3, populacao4, pontosturisticos1, pontosturisticos2, pontosturisticos3, pontosturisticos4;
-    float area1, area2, area3, area4, pib1, pib2, pib3, pib4 
+    float area1, area2, area3, area4, pib1, pib2, pib3, pib4; 
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -20,11 +20,11 @@ int main() {
 
     // Cidade 1
 
-    printf("Bem-vindo ao SUPER TRUNFO PAISES\n\ Insira o CODIGO da primeira CIDADE:\n")
+    printf("Bem-vindo ao SUPER TRUNFO PAISES\n\ Insira o CODIGO da primeira CIDADE:\n");
     scanf("%3s", codigo1);
 
     printf("Insira o NOME da CIDADE:\n");
-    scanf("20s", nome1);
+    scanf("%20s", nome1);
 
     printf("Insira a POPULACAO da CIDADE:\n");
     scanf("%d", &populacao1);
@@ -46,8 +46,4 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
-}
-
-    
-
 }
